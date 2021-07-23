dc6373fa36b18a4d380a6afbba6e32aee8d2da74
RayenD <zufirka@icloud.com>
2021-07-23 04:21:16 +0300

    GEEK-60 [Календарь] Реализовать анимацию раскрытия/скрытия

--- Pills/Pills/Modules/Calendar/CalendarViewController.swift
+++ Pills/Pills/Modules/Calendar/CalendarViewController.swift
@@ -5,25 +5,119 @@
 //  Created by Rayen on 22.07.2021.
 //
 
+//
+//  CalendarViewController.swift
+//  Pills
+//
+//  Created by Rayen on 22.07.2021.
+//
+
 import UIKit
 import FSCalendar
 
 class CalendarViewController: UIViewController {
     
-    fileprivate weak var calendar: FSCalendar!
+    var calendarHeighConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    let showHideButton : UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
-        calendar.scope = .week
-
-        view.addSubview(calendar)
-        self.calendar = calendar
-        calendar.scope = .week
+        view.backgroundColor = .black
+        calendar.backgroundColor = .white
+        calendar.appearance.todayColor = .blue
+        calendar.appearance.selectionColor = .white
+        calendar.appearance.titleSelectionColor = .blue
         calendar.allowsMultipleSelection = true
-//        calendar.firstWeekday = 2
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.scope = .week
+        calendar.scrollDirection = .vertical // bad
+        setConstraints()
+        swipeAction()
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
+        
+    }
+    @objc func showHideButtonTapped() {
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("roll up", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("", for: .normal)
+        }
     }
     
+    func swipeAction() {
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+        
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        
+        switch gesture.direction {
+        case .up:
+            showHideButtonTapped()
+        case .down:
+            showHideButtonTapped()
+        default:
+            break
+        }
+    }
+}
+
+extension CalendarViewController {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeighConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+
+extension CalendarViewController : FSCalendarDataSource, FSCalendarDelegate {
+    
+    func setConstraints() {
+        
+        view.addSubview(calendar)
+        
+        calendarHeighConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeighConstraint)
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+        ])
+        
+        view.addSubview(showHideButton)
+        
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            showHideButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+    }
 }
