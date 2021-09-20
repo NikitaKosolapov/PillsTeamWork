a4550a8c0e17243e7cfb23bc02039e3414f6b2af
Roman Avdonin <gordonbuble49@gmail.com>
2021-09-20 21:15:29 +0300

    GEEK-136-дизайн-реализовать-кастомный-segmented-control

--- Pills/Pills/Common/Extensions/UIImage.swift
+++ Pills/Pills/Common/Extensions/UIImage.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIImage {
     convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
             let rect = CGRect(origin: .zero, size: size)
-            UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 1)
             color.setFill()
             UIRectFill(rect)
             let image = UIGraphicsGetImageFromCurrentImageContext()
